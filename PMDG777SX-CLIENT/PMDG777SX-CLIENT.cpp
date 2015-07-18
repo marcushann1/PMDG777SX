1bc7535e28c15dac859e5ec5f46ab27bda586338
Marcus Hann <marcus.hann6@gmail.com>
2015-07-18 14:13:06 +0100

    add basic networking code
    Add the very base networking code to the program. This will be built
    upon for parsing of received data and sending that data to Prepar3D

--- PMDG777SX-CLIENT/PMDG777SX-CLIENT.cpp
+++ PMDG777SX-CLIENT/PMDG777SX-CLIENT.cpp
@@ -1,11 +1,102 @@
 // PMDG777SX-CLIENT.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#define WINVER                  WindowsVista
+#define _WIN32_WINDOWS          WindowsVista
+#define _WIN32_WINNT            WindowsVista
+#define NTDDI_VERSION NTDDI_VISTA
+#define WINVER _WIN32_WINNT_VISTA
+#define _WIN32_WINNT _WIN32_WINNT_VISTA
 
+
+#include "stdafx.h"
+#include <windows.h>
+
+#define MYPORT "4950" // the port users will be connecting to
+#define MAXBUFLEN 100
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	int err;
+
+	/* Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h */
+	wVersionRequested = MAKEWORD(2, 2);
+
+	err = WSAStartup(wVersionRequested, &wsaData);
+	if (err != 0) {
+		/* Tell the user that we could not find a usable */
+		/* Winsock DLL.                                  */
+		printf("WSAStartup failed with error: %d\n", err);
+		return 1;
+	}
+
+	int sockfd;
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	int numbytes;
+	struct sockaddr_storage their_addr;
+	char buf[MAXBUFLEN];
+	socklen_t addr_len;
+	char s[INET6_ADDRSTRLEN];
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
+	if ((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+	// loop through all the results and bind to the first we can
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+			p->ai_protocol)) == -1) {
+			perror("listener: socket");
+			continue;
+		}
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			closesocket(sockfd);
+			perror("listener: bind");
+			continue;
+		}
+		break;
+	}
+	if (p == NULL) {
+		fprintf(stderr, "listener: failed to bind socket\n");
+		return 2;
+	}
+	freeaddrinfo(servinfo);
+	while (true)
+	{
+		printf("listener: waiting to recvfrom...\n");
+		addr_len = sizeof their_addr;
+		if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN - 1, 0,
+			(struct sockaddr *)&their_addr, &addr_len)) == -1) {
+			perror("recvfrom");
+			exit(1);
+		}
+		printf("listener: got packet from %s\n",
+			inet_ntop(their_addr.ss_family,
+				get_in_addr((struct sockaddr *)&their_addr),
+				s, sizeof s));
+		printf("listener: packet is %d bytes long\n", numbytes);
+		buf[numbytes] = '\0';
+		printf("listener: packet contains \"%s\"\n\n", buf);
+		printf("----------------------------------------\n\n");
+
+		//parse data and send change to P3D
+	}
+
 	return 0;
 }
 
